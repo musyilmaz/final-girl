5b589764bf2c67ec5a0f697c39139f42e932dec6
Mustafa Yilmaz <musyilmaz.dev@gmail.com>
2024-04-16 14:58:13 +0300

    feat(frontend): add tanstack/router

--- apps/frontend/.eslintrc.cjs
+++ apps/frontend/.eslintrc.cjs
@@ -1,16 +1,18 @@
 module.exports = {
   root: true,
-  env: { browser: true, amd: true, node: true, es2020: true },
+  env: { browser: true, es2020: true },
   extends: [
-    "plugin:prettier/recommended",
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ["dist"],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
   rules: {
-    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
   },
 };
--- apps/frontend/src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h1 className="text-3xl text-sky-800">Final Girl</h1>;
-}
-
-export default App;
